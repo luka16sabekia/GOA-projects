2ae364378b1a19c8878c8a60271214d37a70f76b
luka16sabekia <lukasabekia92@gmail.com>
2024-05-26 06:25:54 -0700

    second commit

---
+++ Level05/Classwork/05.py
@@ -0,0 +1,14 @@
+account_balance = 1555
+
+print(account_balance)
+
+#Here is an error because Num is written with a capital letter
+Num = 5
+
+print(Num) 
+
+#we declared variable and gave it name number value is 5 
+number = 10
+number = 5
+number = 15
+print(number)
---
+++ Level05/Homework/lesson05.py
@@ -0,0 +1,38 @@
+# test 2
+# snake case is used here because we can read the variable more easily Two variables, dog_name1 and dog_name2, are initially assigned the values "Rick" and "John", then
+#dog_name2 is reassigned to "Max",
+
+
+
+
+#test3
+first = "hello everybody"
+way2 = f"{first}" 
+print(way2)
+
+#1
+name = "Luka"
+way2 = f"My name is  {name}"
+print(way2)
+
+surname = "sabekia"
+way2 = f"My Surname is {surname}"
+print(way2)
+
+#3
+academy = "GOA"
+way2 = f"I'M learning in { academy }"
+print(way2)
+
+#4
+year = "14"
+way2= f"I'M  {year} years old"                 
+print(way2)
+
+#5
+learn = "programming"
+way2 = f"I'M learning {learn}"
+print(way2)
+
+
+
