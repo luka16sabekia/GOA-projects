f7d02294ad9f60a3310eb40dbfb2aebb82221ae7
luka16sabekia <lukasabekia92@gmail.com>
2024-05-27 13:05:35 -0700

    first commit

---
+++ Level01/Homework/lesson01.py
@@ -0,0 +1,17 @@
+name ="luka"
+surname ="sabekia"
+age =14
+height =155
+print(name +" " +surname)
+print(name +" " + str(age))
+weight=(54)
+origin="Tbilisi"
+hobby="football"
+print (name)
+print (surname)
+print (age)
+print (height)
+print (weight)
+print (origin)
+print (hobby)
+print(name + surname + age + height + weight + origin + hobby) 
---
+++ Level04/Homework/sasaxle.py
@@ -0,0 +1,504 @@
+from turtle import *
+
+speed(20)
+#background color 
+bgcolor("skyblue")
+
+width(5)
+color("green")
+penup()
+goto(-1170 ,-300)
+pendown()
+
+begin_fill()
+forward (1920)
+right (90)
+forward(540)
+right(90)
+forward(540)
+end_fill()
+
+# make sun
+penup()
+goto(-670, 400)
+pendown()
+color("yellow")
+begin_fill()
+circle(50)
+end_fill()
+
+#make cloud
+
+
+penup()
+goto(670, 350)
+pendown()
+color("blue")
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(690, 310)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(710, 350)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(657, 320)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(640, 360)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(655, 290)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(620, 320)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-520, 350)
+pendown()
+color("blue")
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-540, 310)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-560, 350)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-496, 320)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-480, 360)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-510, 290)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+penup()
+goto(-490, 320)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+
+
+
+#draw a castle
+penup()
+goto (550, 50)
+pendown()
+pensize(10)
+color("darkgray", "darkgray") # (stroke, fill)
+begin_fill()  
+end_fill()
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+right(90)
+forward(230)
+
+
+
+
+right(90)
+forward(50)
+
+# make a flag 
+
+
+right(90)
+forward(25)
+left(90)
+forward(50)
+left(90)
+forward(25)
+right(90)
+forward(50)
+left(90)
+begin_fill()
+color("black")
+forward(200)
+right(90)
+forward(210)
+right(90)
+forward(100)
+right(90)
+forward(210)
+end_fill()
+#make G
+begin_fill()
+color("silver")
+penup()
+goto(-260, 440)
+pendown()
+forward(25)
+left (90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(15)
+right(90)
+forward(4)
+end_fill
+#make O
+begin_fill()
+color("green")
+penup()
+goto(-210, 390)
+pendown()
+circle(25)
+end_fill 
+    
+#make A
+begin_fill
+color("silver")
+penup()
+goto(-155 , 390)
+pendown()
+left(70)
+forward(50)
+left(-140)
+forward(30)
+right(110)
+forward(20)
+back(20)
+right(-110)
+forward(18)
+end_fill
+pensize(10)
+color("darkgray", "darkgray") # (stroke, fill)
+
+
+
+penup()
+goto(-450 , 25)
+pendown()
+
+right(20)
+forward (335)
+
+
+left(90)
+forward(500)
+
+
+
+# make a door
+
+begin_fill()
+color("brown")
+left(90)
+forward(167)
+right(90)
+forward(83)
+right(90)
+ 
+
+forward(167)
+right(90)
+forward(83)
+end_fill()
+
+
+
+ 
+
+back(83)
+begin_fill()
+color("darkgray")
+left(180)
+forward(423)
+left(90)
+forward(555)
+left(90)
+forward(15)
+left(90)
+forward(15)
+right(90)
+forward(15)
+right(90)
+forward(15)
+left(90)
+forward(15)
+left(90)
+forward(15)
+right(90)
+forward(15)
+right(90)
+forward(15)
+left(90)
+forward(15)
+left(90)
+forward(15)
+right(90)
+forward(10)
+right(90)
+forward(15)
+left(90)
+forward(25)
+left(90)
+forward(210)
+end_fill
+
+
+#make a N2 flag
+begin_fill()
+color("black")
+
+back(210)
+right(180)
+forward(200)
+left(90)
+forward(280)
+left(90)
+forward(130)
+left(90)
+forward(280)
+end_fill()
+
+#make C
+begin_fill()
+color("white")
+penup()
+goto(200  , 420)
+pendown()
+left(-180)
+forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+
+
+
+#make H
+begin_fill()
+color("green")
+penup()
+goto(230 , 370)
+pendown()
+left(90)
+forward(25)
+right(90)
+forward(25)
+right(90)
+forward(25)
+back(25)
+left(180)
+forward(25)
+back(25)
+right(-90)
+forward(25)
+right(90)
+forward(25)
+
+
+#make A
+begin_fill()
+color("white")
+penup()
+goto(290 , 370)
+pendown()
+right(25)
+forward(50)
+right(140)
+forward(30)
+right(100)
+forward(20)
+back(20)
+left(95)
+forward(17)
+
+
+#make D
+begin_fill()
+color("green")
+penup()
+end_fill()
+goto(360,365)
+pendown()
+left(172)
+forward(50)
+penup()
+goto(360 ,365)
+pendown()
+left(-90)
+circle(25, 170)
+
+#make HANDLE
+begin_fill()
+color("black")
+penup()
+goto(68, -217)
+pendown()
+circle(5)
+end_fill()
+
+
+#draw TREE
+begin_fill()
+color("brown")
+penup()
+goto(-600 , -70)
+pendown()
+right(-7)
+forward(80)
+left(90)
+forward(245)
+left(90)
+forward(80)
+left(90)
+forward(245)
+end_fill()
+
+
+
+
+#make leaves
+begin_fill()
+color("green")
+penup()
+goto(-560 , -0.1)
+circle(80 ,)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
