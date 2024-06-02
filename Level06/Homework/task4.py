8563d56eb6b4256f5240026b16845cd804ada31a
luka16sabekia <lukasabekia92@gmail.com>
2024-06-02 12:01:54 -0700

    sixth commit

--- Level06/Homework/task4.py
+++ Level06/Homework/task4.py
@@ -4,6 +4,7 @@ last_name = input("Please enter your last name: ")
 age = input("Please enter your age: ")
 password = input("Please enter your password: ")
 
+
 # User result
 print("User Account:")
 print(f"First Name: {first_name}")
