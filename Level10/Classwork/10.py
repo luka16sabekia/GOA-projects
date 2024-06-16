7b11d69d16cbb7ca92e4a1509f0511420e4c8976
luka16sabekia <lukasabekia92@gmail.com>
2024-06-16 06:13:27 -0700

    commit

---
+++ Level10/Classwork/10.py
@@ -0,0 +1,22 @@
+#task 1
+
+for i in range(50, 101):
+    print(i)
+
+
+#task2
+
+for i in range(10, 0, -1):
+    print(i)
+
+#task3
+
+for i in range(100):
+    print("Hello world")
+
+
+#task4
+
+for i in range(1, 11):
+    print(i)
+    print("Hello world")
---
+++ Level10/Homework/task4.py
@@ -0,0 +1,9 @@
+"""4) while loopის გამოყენებით გამოიტანეთ 1დან 10მდე რიცხვების ჯამი."""
+
+
+i = 1
+num = 0
+while i <= 10:
+    num += i
+    i += 1
+print("The sum of the numbers from 1 to 10 is:", num)
