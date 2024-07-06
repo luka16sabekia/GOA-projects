f355d9a6319d6b57e6f6c0e81b60dd51e1108b8c
luka16sabekia <lukasabekia92@gmail.com>
2024-07-06 05:57:07 -0700

    commit

---
+++ Level16/Classwork/classwork.py
@@ -0,0 +1,40 @@
+#task1 
+list1 = [1,2,3,4,5,6,7,8,9,10]
+        #0,1,2,3,4,5,6,7,8,9
+print(list1)
+
+# task2
+list1 = [1,2,3,4,5,6,7,8,9,10]
+        #0,1,2,3,4,5,6,7,8,9
+first = list1[:3]
+second = list1[7:]
+third = list1[3:7]
+print(first)
+print(second)
+print(third)
+
+# task3
+list1 = [1,2,3,4,5,6,7,8,9,10]
+        #0,1,2,3,4,5,6,7,8,9
+first1 = list1[-10:-7]
+second1 = list1[-3:]
+third1 = list1[-7:-3]
+print(first1)
+print(second1)
+print(third1)
+
+# task4
+
+list2 = [11,15,22,33,65,90]
+         #0, 1, 2, 3, 4, 5
+for i in list2:
+    print(i)
+ 
+
+# task5
+
+list3 = "Hello World"
+
+for i in list3:
+    print(i)
+
