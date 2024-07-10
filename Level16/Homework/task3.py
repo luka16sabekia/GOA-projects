9e9a031384deefa38313658ccb0cc373cbd80a11
luka16sabekia <lukasabekia92@gmail.com>
2024-07-10 00:05:59 -0700

    commit

---
+++ Level16/Homework/task3.py
@@ -0,0 +1,9 @@
+"""3) შექმენით string'ების list'ი, გადაატარეთ for loopი
+და გამოიტანეთ ყველა სიტყვა თავში "#"ით.
+მაგალითად: list1 = ["data", "luka"]"""
+
+name = ["data", "luka"]
+
+
+for i in name:
+    print("#" + i)
---
+++ Level16/Homework/task5.py
@@ -0,0 +1,20 @@
+"""5) შექმენით string'ი 10 სიტყვით და ცალ-ცალკე შექმენით მათი 
+სიტყვების ცვლადები slicing'ის მეშვეობით, როგორც საკლასო მეექვსე დავალებაში"""
+
+sentence = "This is the coolest online academy GOA"
+
+word1 = sentence[0:4]      
+word2 = sentence[5:7]    
+word3 = sentence[8:11] 
+word4 = sentence[12:19]    
+word5 = sentence[20:26]     
+word6 = sentence[27:34]     
+word7 = sentence[35:38]   
+
+print(word1)
+print(word2)
+print(word3)
+print(word4)
+print(word5)
+print(word6)
+print(word7)
