7190ae7fb7542a6f2262163cb03b420cf17deba6
luka16sabekia <lukasabekia92@gmail.com>
2024-08-05 04:16:10 -0700

    commit

---
+++ Level24/Homework/task1.py
@@ -0,0 +1,7 @@
+def average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+print((numbers)) 
---
+++ Level24/Homework/task2.py
@@ -0,0 +1,7 @@
+def manual_abs(value):
+    if value < 0:
+        return -value
+    return value
+
+print((-5))
+print((3))  
